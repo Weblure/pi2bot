08f89178bcf51ecddea59c8c2309f84b16834f54
ngbeslhang <ngbeslhang@gmail.com>
2016-01-11 20:16:06 +0800

    v0.1
    New features:
    Ownership system
    
    New commands:
    [O] - Owner-only
    
    enable - Enable the bot. [O]
    disable - Disable the bot. [O]
    kill - Kill the script's process. [O]
    check <status> - Detect the second parameter and determine which to
    check. [O] (Might move to public commands)
    hello - Says hello back.

--- Python 3/text.py
+++ Python 3/text.py
@@ -1,10 +1,11 @@
 # text.py - Part of pi2bot, by ngbeslhang
 # NOTE: Those with * are done.
 # TO-DO LIST:
-#   - Bot kill and start command for the bot and add a trigger function + boolean.
+#   * Global bot disabling and enabling command. (Kudos to Hornwitser)
+#       - Able to allow owner to disable individual commands
 #       - Which means a bot can be killed yet staying online so that owner can restart it.
-#       * Will also include totalkill command (closing the entire Python script and make bot offline)
-#   - Ownersnip system for certain commands w/ owner-checking function which returns boolean value. (Inspired by SexualRhinoceros' Musicbot)
+#       * Kill command (closing the entire Python script and make bot offline)
+#   * Ownership system for certain commands. (Inspired by SexualRhinoceros' Musicbot)
 #       - Which also allow creation of whitelist command for certain commands.
 #           - ...which also leads to an idea of bot's permission system.
 #           - Command that toggles no permission message, disabled by default.
@@ -12,9 +13,21 @@
 #   - Bot language switcher.
 #   - Command that allow owner/whitelist to disable and enable other commands (not itself)
 #   - Command that allow owner to add a new command for simple purpose/code. (If possible)
+#   - PM-based invitation accept system which the owner can choose to accept or not.
+#   - Help command which also list help for individial command if inserted as second parameter.
+#       - Inspired by Red_M testing his bot, Frog's ,help command
+#
+# Kudos to Hornwitser, Nerketur and others at unofficial Discord API server's #python_discord-py for helping me teaching me more about Python!
 
 import discord
 import asyncio
+
+# The global variables below are either from main() or for Ownership/Permission system
+# They are put here for easiness to access and editing (due to extensive testing)
+# REMEMBER TO CHANGE ALL OF THEM
+email = 'email'
+password = 'passwd'
+owner_id = 'id'
 
 bot = discord.Client()
 
@@ -24,50 +37,94 @@ def on_ready():
     print('Connected!')
     print('\tDiscord username: ' + bot.user.name)
     print('\tDiscord ID: ' + bot.user.id)
-
+    
+is_disabled = False
+disabled_msg_count = 0
+    
 @bot.async_event
 def on_message(message):
+    # Used to prevent "referenced before assignment" error, Hornwitser
+    global is_disabled
+    global disabled_msg_count
+    
     # From discord.py's example codes, returns if the message author is bot itself.
     if message.author == bot.user:
         return
     
     # pi2bot uses @mention command prefix, thought might add other custom prefix templates outside bot's code.
-    # Kudos to Hornwitser and Nerketur for helping me on prefix and teaching me more about Python!
+    # Hornwitser
     if len(message.mentions) == 1 and message.mentions[0] == bot.user:
         if message.content.startswith(bot.user.mention):
             cmd = message.content[len(bot.user.mention)+1:]
             
-            # Commands
-            # Display your Discord ID on console
-            if cmd == 'myid':
-                print(message.author.id)
-            # kill - Kill the bot's Python script process
-            if cmd == 'kill':
-                if check_ownership(message, cmd, True) == True:
-                    yield from bot.send_message(message.channel, "Forcekilling the bot.")
-                    bot.close()
-                    loop.close()
+            is_owner = check_ownership(message, cmd)
+            # "cmd not in ()", Hornwitser
+            if cmd not in ('enable', 'disable', 'check status') and is_disabled == True:
+                if is_owner == True:
+                    yield from bot.send_message(message.channel, 'Currently disabled, please reenable by typing \"' + bot.user.mention + ' enable\".')
+                # Set the count "disabled" message will display before it reach the number and stop.
+                elif disabled_msg_count != 1:
+                    yield from bot.send_message(message.channel, 'Currently disabled, please contact the bot owner to reenable.')
+                    disabled_msg_count = disabled_msg_count + 1
+                return
+            
+            # This part is for owner-only commands.
+            if is_owner == True and cmd in ('kill', 'enable', 'disable') or cmd[:5] in ('check'):
+                if cmd == 'kill':
+                    yield from bot.send_message(message.channel, 'Killing...')
+                    cmd_kill()
+                if cmd == 'enable' or 'disable':
+                    if cmd == 'disable':
+                        yield from bot.send_message(message.channel, 'Disabling...')
+                        is_disabled = bot_toggle(cmd)
+                    elif cmd == 'enable':
+                        yield from bot.send_message(message.channel, 'Enabling...')
+                        is_disabled = bot_toggle(cmd)
+                        disabled_msg_count = 0
+                if cmd[:5] == 'check':
+                    if cmd[6:] == 'status':
+                        if is_disabled == True:
+                            status = 'Disabled'
+                            disabled_msg_count = disabled_msg_count + 1
+                        else:
+                            status = 'Enabled'
+                        yield from bot.send_message(message.channel, 'Bot status: ' + status)
+                        
+            elif is_owner == False and cmd in ('kill', 'enable', 'disable') or cmd[:5] in ('check'):
+                yield from bot.send_message(message.channel, 'Sorry ' + message.author.mention + ', you do not have permission to use the command.')
+            
+            # This part is for public commands.
+            else:
+                if cmd == 'hello':
+                    yield from bot.send_message(message.channel, 'Hello, ' + message.author.mention + '!')
+                
+# Bot toggle
+def bot_toggle(cmd):
+    if cmd == 'enable':
+        return False
+    elif cmd == 'disable':
+        return True
+                    
+# Command functions
+def cmd_kill():
+    loop.run_until_complete(bot.close())
+    loop.close()
+                    
 # Ownership/Permission system
 # check_ownership() - Check the ID of message's author and compare it with a string.
 # no_perm() - Send message to user that s/he do not have permission to use the command.         
 def check_ownership(msg, cmd, display_msg = False):
-    # Remember to add ID into owner.
-    owner = ''
-    if msg.author.id == owner:
+    if msg.author.id == owner_id:
         print('Owner ' + msg.author.name + '(' + msg.author.id + ') is using ' + cmd + '.')
         return True
     else:
-        if display_msg == True:
-            no_perm(msg)
-
-def no_perm(msg):
-    bot.send_message(msg.channel, 'Sorry ' + msg.author.mention + ', you do not have permission to use the command.')
+        print(msg.author.name + ' wanted to use ' + cmd + ' but do not have permission to do so.')
+        return False
             
 # The part below is from discord.py's quickstart.
 def main():
     print('Connecting...')
-    # REMEMBER TO EDIT THE CODE BELOW!
-    yield from bot.login('email', 'password')
+    yield from bot.login(email, password)
     yield from bot.connect()
     
 loop = asyncio.get_event_loop()
